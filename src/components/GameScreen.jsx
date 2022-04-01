17091805d1bb9830241a7a05cc6f077fc9c45a63
Johan Ekman <ekman-johan020@hotmail.com>
2022-04-01 15:56:44 +0200

    Did even more stuff

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -1,10 +1,14 @@
 import { getData } from '../services/questionService';
+import { useState, useEffect } from 'react';
+import Question from './Question';
 
 const GameScreen = ({ data }) => {
-  getData(data);
+  const [counter, setCounter] = useState(0);
+
   return (
     <div>
       <h1>Game</h1>
+      {counter < data.length && <Question category={data[counter].category} />}
     </div>
   );
 };
