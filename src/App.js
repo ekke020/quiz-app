b897bc653298efda51c542f8691e344a33024d4d
Johan Ekman <ekman-johan020@hotmail.com>
2022-04-01 14:00:49 +0200

    testing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Welcome from './components/Welcome';
 import { getData } from './services/questionService';
-import { useState } from "react";
+import { useState } from 'react';
 import GameScreen from './components/GameScreen';
 
-
 function App() {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState(null);
+  console.log('log');
   return (
     <div>
-      {data ? <GameScreen data={data}/> : <Welcome setData={setData} />}
+      {data ? <GameScreen data={data} /> : <Welcome setData={setData} />}
     </div>
   );
 }
