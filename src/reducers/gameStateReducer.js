55bd07002a6c6fdeef08d6763475df5a5e9b605f
Johan Ekman <ekman-johan020@hotmail.com>
2022-04-05 14:26:02 +0200

    Added the loading screen

--- src/reducers/gameStateReducer.js
+++ src/reducers/gameStateReducer.js
@@ -1,7 +1,7 @@
-import Welcome from '../components/Welcome';
+import Welcome from '../components/StartScreen';
 import GameScreen from '../components/GameScreen';
-import { useSelector } from 'react-redux';
 import ResultScreen from '../components/ResultScreen';
+import LoadingScreen from '../components/LoadingScreen';
 
 export const setGameState = (gameState) => {
   return {
@@ -11,19 +11,17 @@ export const setGameState = (gameState) => {
 
 const gameStateReducer = (state = <Welcome />, action) => {
   switch (action.type) {
-    case 'WELCOME_SCREEN':
+    case 'START_SCREEN':
       return <Welcome />;
-
     case 'GAME_SCREEN':
       return <GameScreen />;
-
     case 'RESULT_SCREEN':
       return <ResultScreen />;
-
+    case 'LOADING_SCREEN':
+      return <LoadingScreen />;
     default:
       return state;
   }
 };
 
 export default gameStateReducer;
-
