a26364c5abd4b3f3babde0d07b21ca300c95c754
Johan Ekman <ekman-johan020@hotmail.com>
2022-04-05 16:36:35 +0200

    Imported style module

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { useSelector } from 'react-redux';
+import styles from './css/app.module.css';
 
 function App() {
   const state = useSelector((state) => state.gameState);
 
-  return <div>{state}</div>;
+  return <div className={styles.gameContainer}>{state}</div>;
 }
 
 export default App;
