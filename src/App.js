973084038608de611f5b45311d77593e87b77197
Johan Ekman <ekman-johan020@hotmail.com>
2022-04-05 20:19:35 +0200

    Removed depricated CSS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import styles from './css/app.module.css';
 function App() {
   const state = useSelector((state) => state.gameState);
 
-  return <div className={styles.gameContainer}>{state}</div>;
+  return (
+    <div>
+      <div className={styles.gameContainer}>{state}</div>
+    </div>
+  );
 }
 
 export default App;
