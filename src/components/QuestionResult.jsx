5245bccfb1daee3e36af3f544e5a5df102a3e1e3
Johan Ekman <ekman-johan020@hotmail.com>
2022-04-06 08:51:47 +0200

    Added css

--- src/components/QuestionResult.jsx
+++ src/components/QuestionResult.jsx
@@ -1,8 +1,24 @@
-const QuestionResult = ({ question }) => {
+import styles from '../css/scoreScreen.module.css';
+import { useState } from 'react';
+
+const QuestionResult = ({ q }) => {
+  const answerResult = () => (q.answeredCorrect ? 'correct' : 'incorrect');
+  const [visible, isVisible] = useState(false);
+
+  const extraInfo = () => (
+    <>
+      <p>Category: {q.category}</p>
+      {!q.answeredCorrect && <p>Correct answer: {q.correctAnswer}</p>}
+    </>
+  );
   return (
-    <div>
-      <p>{question.question}</p>
-      <p>Your answer was {question.answeredCorrect.toString()}</p>
+    <div
+      onClick={() => isVisible(!visible)}
+      className={`${styles.question} ${q.answeredCorrect && styles.correct}`}
+    >
+      <p>{q.question}</p>
+      <p>Your answer was {answerResult()}</p>
+      {visible && extraInfo()}
     </div>
   );
 };
